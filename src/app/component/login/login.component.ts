b7414d659a5383f1de641e2adbbee161a5ca043e
sushmitabudhathoki <sushmita.budhathoki56@gmail.com>
2024-08-28 01:46:54 -0400

    Home page, Login page

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  form: FormGroup = new FormGroup({});
+  constructor(
+    private builder: FormBuilder
+  ) {
+  }
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.form = this.builder.group({
+      username: [undefined, Validators.required],
+      password: [undefined, Validators.required]
+    })
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      alert("Please fill out form")
+    } else {
+      console.log("submitted data", this.form.value);
+
+    }
+  }
 
 }
